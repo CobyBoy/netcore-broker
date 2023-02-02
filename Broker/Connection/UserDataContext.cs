9d723b018a66e4521786b5098902534a20abb313
CobyBoy <aboutacoby@gmail.com>
2023-02-02 20:14:11 -0300

    Added user data context

---
+++ Broker/Connection/UserDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using BrokerApi.Models;
+
+namespace BrokerApi.Connection
+{
+    public class UserDataContext : DbContext
+
+    {
+
+        public UserDataContext(DbContextOptions<UserDataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
