6db290a893ce90b5008832c7c3fe28f316c2d2ac
CobyBoy <aboutacoby@gmail.com>
2023-02-02 20:13:51 -0300

    Added userdto

---
+++ Broker/DTOs/UserDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrokerApi.DTOs
+{
+    public class UserDto
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MinLength(4, ErrorMessage = "Password must be 4 characters at least")]
+        [MaxLength(16, ErrorMessage = "Password must be 16 characters maximum")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{4,}$", ErrorMessage = "Password must be uppercase, lowercase, a special character and a number")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required, MinLength(4, ErrorMessage = "Username must be 4 characters at least")]
+        [MaxLength(16, ErrorMessage = "Password must be 16 characters maximum")]
+        [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]{3,10}$", ErrorMessage = "Username must contain at least a number")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression("[0-9]+", ErrorMessage = "Please provide only numbers for area code")]
+        public string AreaCode { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression("[0-9]+", ErrorMessage = "Please provide only numbers for phone number")]
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
