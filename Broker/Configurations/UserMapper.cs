d6681e5ae772286d8a7bc005df3b0bf8689717e3
CobyBoy <aboutacoby@gmail.com>
2023-02-15 23:15:59 -0300

    Added mapper

---
+++ Broker/Configurations/UserMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BrokerApi.DTOs;
+using BrokerApi.Models;
+
+namespace BrokerApi.Configurations
+{
+    public class UserMapper: Profile
+    {
+        public UserMapper()
+        {
+           CreateMap<User,RegisterUserDto>().ReverseMap();
+        }
+    }
+}
